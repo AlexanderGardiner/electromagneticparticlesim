cc5ab1c3da72cef2f5ba4aac17963da6934bd845
Alex Gardiner <>
2022-05-19 18:27:31 +0000

    Backup

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ let particles = [];
 let magneticFields = [];
 
 class particle {
-  constructor(x,y,width, height, mass, charge) {
+  constructor(x,y,width, height, mass, charge,xVelocity,yVelocity) {
     this.x = x;
     this.y = y;
-    this.xVelocity = 5;
-    this.yVelocity = 0;
+    this.xVelocity = xVelocity;
+    this.yVelocity = yVelocity;
     this.width = width;
     this.height = height;
     this.mass = mass;
@@ -47,31 +47,34 @@ class electricField {
   }
 }
 function Start() {
-  let particle1 = new particle(0,300,30,30,100,-1);
-  let particle2 = new particle(0,300,30,30,100,1);
+  let particle1 = new particle(0,300,30,30,100,-1,5,8);
+  let particle2 = new particle(0,300,30,30,100,0.5,5,-17);
 
   particles.push(particle1);
   particles.push(particle2);
-  let magneticField1 = new magneticField(300,100,700,200,10);
+  let magneticField1 = new magneticField(300,100,500,350,10);
 
-  let electricField1 = new electricField(100,200,700,200,50,1);
+  let electricField1 = new electricField(0,200,viewCanvas.width,150,30,1);
   requestAnimationFrame(function() {
     Update(particles,magneticField1,electricField1,performance.now());
   });
 }
 function Update(particles,magneticField1, electricField1,lastTime) {
   currentTime = performance.now()
-  deltaTime = (currentTime - lastTime)/18;
-  
-  ClearCanvas();
+  deltaTime = (currentTime - lastTime)/40;
+  viewCanvasCTX.fillStyle = "#808080";
+  viewCanvasCTX.globalAlpha = 0.07;
+  viewCanvasCTX.fillRect(0, 0, viewCanvas.width, viewCanvas.height)
+  viewCanvasCTX.globalAlpha = 1;
+  //ClearCanvas();
 
   viewCanvasCTX.fillStyle = "#FF0000";
-  viewCanvasCTX.globalAlpha = 0.2;
+  viewCanvasCTX.globalAlpha = 0.007;
   viewCanvasCTX.fillRect(magneticField1.x, viewCanvas.height-magneticField1.height-magneticField1.y, magneticField1.width, magneticField1.height)
   viewCanvasCTX.globalAlpha = 1;
 
   viewCanvasCTX.fillStyle = "#FFF000";
-  viewCanvasCTX.globalAlpha = 0.2;
+  viewCanvasCTX.globalAlpha = 0.07;
   viewCanvasCTX.fillRect(electricField1.x, viewCanvas.height-electricField1.height-electricField1.y, electricField1.width, electricField1.height)
 
   for (let i = 0; i < particles.length; i++) { 
@@ -88,13 +91,7 @@ function Update(particles,magneticField1, electricField1,lastTime) {
     particles[i].x+=particles[i].xVelocity*deltaTime;
     particles[i].y+=particles[i].yVelocity*deltaTime;
   }
-  /*particle1.inMagneticField = DetectCollision(particle1,magneticField1)
 
-  particle1.inElectricField = DetectCollision(particle1,electricField1)
-  
-  particle1 = wallCollision(particle1);
-  particle1 = UpdateVelocities(particle1,magneticField1,electricField1,deltaTime);
-  */
   
   
 

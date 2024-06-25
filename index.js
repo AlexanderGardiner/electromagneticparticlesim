903b18c13db0176658370241b34eac26083d54ed
AlexanderGardiner <misc@alexandergardiner.com>
2024-06-25 12:28:08 -0700

    Updates to path

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3010;
 
-app.use(express.static("public"));
+app.use(
+  "/electromagneticparticles",
+  express.static(path.join(__dirname, "public"))
+);
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
